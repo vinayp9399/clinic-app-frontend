4200736b2526fe29adf87aafcd8e6a3bdcb652c9
Vinay Phatarpekar <vinayp9399@gmail.com>
2024-09-01 15:20:28 +0530

    done

--- src/components/home/landing.jsx
+++ src/components/home/landing.jsx
@@ -10,7 +10,7 @@ const Landing=()=>{
         <div class="container_13">
             <h2>Welcome to E-Clinic</h2>
             <p>Your health is our priority. Book an appointment with our <br/>experienced doctors today.</p>
-            <a onClick={()=>{navigate("/services")}} class="btn">Our Services</a>
+            <a onClick={()=>navigate('/doctorlist')} class="btn">Book Appointment</a>
         </div>
     </section>
 
@@ -43,7 +43,7 @@ const Landing=()=>{
                                 <div style={{width:"200px"}} className="col-xl-4 d-flex align-items-stretch">
                                     <div className="icon-box mt-4 mt-xl-0">
                                         
-                                        <h4>Emegency Cases</h4>
+                                        <h4>Emergency Cases</h4>
                                         <h6 className='text-secondary'>+88 01751 040425</h6>
                                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facilis perferendis quia maxime. Laborum excepturi pariatur laboriosam nihil, dolor molestias.</p>
                                     </div>
@@ -54,9 +54,9 @@ const Landing=()=>{
                                         <h4>Working Hours</h4>
                                         <small className='text-secondary'>Timing schedule</small>
                                         <ul className='list-group list-group-flush'>
-                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Sun - Wed : </p> <p>8:00 - 17: 00</p></li>
-                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Thus - Fri : </p> <p>9:00 - 17: 00</p></li>
-                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Sat - Sun : </p> <p>10:00 - 17: 00</p></li>
+                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Sun - Wed : </p> <p>8:00 - 17:00</p></li>
+                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Thus - Fri : </p> <p>9:00 - 17:00</p></li>
+                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Sat - Sun : </p> <p>10:00 - 17:00</p></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -71,7 +71,7 @@ const Landing=()=>{
     <section>
         <div style={{backgroundColor:"transparent"}} id="services1" class="container_12">
             <h2>Our Doctors Specializations</h2>
-            <div class="services-grid">
+            <div style={{marginBottom:"50px"}} class="services-grid">
                 <div class="service">
                     <i class="fas fa-stethoscope"></i>
                     <h3 style={{color:"#1977cc"}}>General Check-up</h3>
@@ -93,23 +93,26 @@ const Landing=()=>{
                     <p>Quality care for your little ones.</p>
                 </div>
             </div>
-            <button onClick={()=>navigate('/services')} style={{cursor:"pointer",marginBottom:"50px",marginTop:"40px"}} class="btnp">Our Services</button>
+            {/* <button onClick={()=>navigate('/services')} style={{cursor:"pointer",marginBottom:"50px",marginTop:"40px"}} class="btnp">Our Services</button> */}
         </div>
     </section>
 
-    <section style={{backgroundColor:""}} id="about1">
-        <div class="container_12">
+    <section style={{display:"flex",padding:"63px"}} id="about1">
+        <div><img style={{borderRadius:"10px"}} src="https://lh5.googleusercontent.com/proxy/D9U14usXzmBtcFV5EAMM4RuvPeVhPc5zaIXoV2ahGY_RDWNyzgbSwr0EIoqM93DLUcEia4u4YJQfjYZQI4yQK6VzA12lMZfA_0C6mN-wxH_gsVukHPbCOMraqlE" alt="" /></div>
+        <div style={{textAlign:"left"}} class="container_12">
             <h2>About Us</h2>
-            <p>E-Clinic has been provides top-notch healthcare services through easy appouintment booking. Our team of experienced doctors are dedicated to ensuring the best possible care for our patients.</p>
-        </div>
-        <button onClick={()=>navigate('/aboutus')} style={{cursor:"pointer",marginTop:"50px"}} class="btnp">Know More</button>
+            <p>E-Clinic has been provides top-notch healthcare services through easy appouintment booking. Our team of experienced doctors are dedicated to ensuring the best possible care for our patients. We provide best leading medicle service Nulla perferendis veniam deleniti ipsum officia dolores repellat laudantium obcaecati neque.</p>
+        
+        <button onClick={()=>navigate('/aboutus')} style={{cursor:"pointer",marginTop:"30px"}} class="btnp">Know More</button></div>
     </section>
-    <section style={{backgroundColor:"white"}} id="doctors1">
-        <div class="container_12">
+    <section style={{backgroundColor:"white",display:"flex",padding:"63px",gap:"100px"}} id="doctors1">
+        <div style={{textAlign:"left"}}>
             <h2>Our Doctors</h2>
-            <p>Our team of experienced doctors are dedicated to ensuring the best possible care for our patients.</p>
-        </div>
-        <button onClick={()=>navigate('/doctorlist')} style={{cursor:"pointer",marginTop:"50px"}} class="btnp">Book our Doctors</button>
+            <p>Our team of experienced doctors are dedicated to ensuring the best possible care for our patients. Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover Various versions have evolved over the years, sometimes. <br/><br />
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.</p>
+            <button onClick={()=>navigate('/doctorlist')} style={{cursor:"pointer",marginTop:"20px"}} class="btnp">Book Appointment</button>
+        </div>        
+        <div><img style={{borderRadius:"10px"}} width="500px" src="https://longislandneuro.com/wp-content/uploads/2023/01/handsome-male-doctor-explaining-medical-treatment-2022-12-15-18-17-30-utc.webp" alt="" /></div>
     </section>
 
     
